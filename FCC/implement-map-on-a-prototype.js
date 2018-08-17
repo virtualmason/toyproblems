1a60e5a55741133c759fa278189e726a3eb2a5a7
Floyd Orr <floydcanhelp@gmail.com>
2018-08-16 19:15:45 -0500

    Update implement-map-on-a-prototype.js

--- FCC/implement-map-on-a-prototype.js
+++ FCC/implement-map-on-a-prototype.js
@@ -2,14 +2,13 @@ var s = [23, 65, 98, 5];
 Array.prototype.myMap = function(callback) {
   var newArray = [...s];
   let near = [];
-  newArray.forEach(function(elm) {
-    near.push(callback(elm));
-    n;
-  }); // Add your code above this linen  return near;};
+     newArray.forEach(function(elm) {
+     near.push(callback(elm));
+   }); // Add your code above this linen  return near;};
 
-  var new_s = s.myMap(function(item) {
+ var new_s = s.myMap(function(item) {
     return item * 2;
-  });
+ });
   console.log(new_s + " new_s");
 };
-s;
+
